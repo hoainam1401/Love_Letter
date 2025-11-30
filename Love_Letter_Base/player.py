0b0fbec7c63aa7b27921aef47e85f0eddef6200d
Duc Anh Le <jhindeag@gmail.com>
2025-11-30 16:37:04 +0100

    added card logic

--- Love_Letter_Base/player.py
+++ Love_Letter_Base/player.py
@@ -1,6 +1,9 @@
+from card import Card
+
+
 class Player:
     name: str = "Player 1"
-    hand: list = []
+    hand: list[Card] = []
     isKO: bool = False
     hasHandmaid: bool = False
     finalPoint: int = 0
@@ -8,3 +11,14 @@ class Player:
 
     def __init__(self, name: str):
         self.name = name
+        self.hand: list[Card] = []
+        self.isKO: bool = False
+        self.hasHandmaid: bool = False
+        self.finalPoint: int = 0
+        self.winningTokenCount: int = 0
+
+    def showCards(self):
+        s = ""
+        for card in self.hand:
+            s += card.name + " "
+        return s
