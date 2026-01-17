0cddafdcaa171636b5a0c80e1b6034971e4c33ba
Duc Anh Le <jhindeag@gmail.com>
2026-01-17 01:23:41 +0100

    modified for deploying

--- Love_Letter_Backend/Love_Letter_Backend/settings.py
+++ Love_Letter_Backend/Love_Letter_Backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
@@ -96,10 +97,15 @@ WSGI_APPLICATION = "Love_Letter_Backend.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    "default": dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default="postgresql://postgres:postgres@localhost:5432/mysite",
+        conn_max_age=600,
+    )
 }
 
 
